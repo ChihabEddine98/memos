b8804e98404e3ee7a38f2776843e70b8bd295a62
Chihab Eddine Benamara <ga_benamara@esi.dz>
2020-03-05 05:32:26 -0800

    show memo list but static input TODO...

--- src/controllers/memos.js
+++ src/controllers/memos.js
@@ -1,3 +1,38 @@
+let memos=[ 
+    {title : 'Objet Trouvé ',
+     imgUrl : 'https://images.unsplash.com/photo-1502759683299-cdcd6974244f?auto=format&fit=crop&w=440&h=220&q=60',
+     description :"Le Lorem Ipsum est rression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécim",
+    },
+    {title : 'Autre Truc',
+    imgUrl : 'https://images.unsplash.com/photo-1502759683299-cdcd6974244f?auto=format&fit=crop&w=440&h=220&q=60',
+    description :"Le Lorem Ipsum est rression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécim",
+   },
+   {title : 'Autre Truc',
+   imgUrl : 'https://images.unsplash.com/photo-1502759683299-cdcd6974244f?auto=format&fit=crop&w=440&h=220&q=60',
+   description :"Le Lorem Ipsum est rression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécim",
+  },
+  {title : 'Autre Truc',
+  imgUrl : 'https://images.unsplash.com/photo-1502759683299-cdcd6974244f?auto=format&fit=crop&w=440&h=220&q=60',
+  description :"Le Lorem Ipsum est rression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécim",
+ },
+ {title : 'Autre Truc',
+ imgUrl : 'https://images.unsplash.com/photo-1502759683299-cdcd6974244f?auto=format&fit=crop&w=440&h=220&q=60',
+ description :"Le Lorem Ipsum est rression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécim",
+},
+{title : 'Autre Truc',
+imgUrl : 'https://images.unsplash.com/photo-1502759683299-cdcd6974244f?auto=format&fit=crop&w=440&h=220&q=60',
+description :"Le Lorem Ipsum est rression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécim",
+},
+{title : 'Autre Truc',
+imgUrl : 'https://images.unsplash.com/photo-1502759683299-cdcd6974244f?auto=format&fit=crop&w=440&h=220&q=60',
+description :"Le Lorem Ipsum est rression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécim",
+}
+]
+
+exports.getMemos=((req,res,next)=>
+{
+    res.render('../views/memos.ejs',{ pageTitle :'Mémos !',memos:memos})
+})
 
 exports.getAddMemo=((req,res,next)=>
 {
--- src/routes/memo.js
+++ src/routes/memo.js
@@ -2,6 +2,8 @@ const express=require('express')
 const memoController=require('../controllers/memos')
 const router=express.Router()
 
-router.get('/',memoController.getAddMemo)
+
+router.get('/add',memoController.getAddMemo)
+router.get('/',memoController.getMemos)
 
 module.exports=router
