b51d4eb8069c5c67c2d75c12c8037e590f65bcb6
Chihab Eddine Benamara <ga_benamara@esi.dz>
2020-03-11 08:10:32 -0700

    add register a new user

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,4 +1,4 @@
-
+const User = require('../models/User')
 
 exports.getLogin =((req,res,next)=>{
     res.render('../views/auth/login.ejs',{ pageTitle :'Connection à Mémos'})
@@ -9,11 +9,44 @@ exports.getRegister =((req,res,next)=>{
 })
 
 exports.postRegister =((req,res,next)=>{
+    const nom=req.body.nom
+    const prenom =req.body.prenom
     const email=req.body.email
     const password=req.body.password
     const confirm=req.body.confirmPassword
 
+    User.findOne({where :{email:email}})
+    .then((user) => {
+        if(!user)
+        {
+            User.create({
+                email:email,
+                password : password,
+                first_name: prenom,
+                last_name:nom
+            })
+
+            res.render('../views/auth/login.ejs',{ pageTitle :'Connection à Mémos'})
+        }
+        else  // Exist Déja !
+        {
+            console.log('connectez vous !')    
+        }
+            
+    }).catch((err) => {
+        
+    });
+
+
+
     res.render('../views/auth/login.ejs',{ pageTitle :'Connection à Mémos'})
 })
+
+
+
+
+
+
+
 
 
