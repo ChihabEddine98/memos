fa4b27413b7ca5877f785e01353743abd0ef7c38
abenam27 <ga_benamara@esi.dz>
2020-03-23 14:34:01 +0100

    front use select mobuis-selectr

--- src/controllers/admin.js
+++ src/controllers/admin.js
@@ -42,7 +42,6 @@ exports.getMemos=((req,res,next)=>
                       memos:memos,
                       isAuth: req.session.isLoggedIn,
                       canShare:false,
-                      userId:req.user.id,
                       user : req.user,
                       isAdmin :req.session.isAdmin
                     })
